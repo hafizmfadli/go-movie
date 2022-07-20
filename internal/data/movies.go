9502972e53c627949d4dec59b4fe1538b01c1abe
hafizmfadli <lshafizmf1st@gmail.com>
2022-07-20 17:54:40 +0700

    add movie model

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,9 +1,13 @@
 package data
 
 import (
+	"context"
+	"database/sql"
+	"errors"
 	"time"
 
 	"github.com/hafizmfadli/go-movie/internal/validator"
+	"github.com/lib/pq"
 )
 
 type Movie struct {
@@ -35,4 +39,85 @@ func ValidateMovie(v *validator.Validator, movie *Movie) {
 	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
 	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
 	v.Check(validator.Unique(movie.Genres), "genres", "must not contain dulicate values")
+}
+
+type MovieModel struct {
+	DB *sql.DB
+}
+
+// Insert inserting a new record in the movies table
+func (m MovieModel) Insert(movie *Movie) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3 * time.Second)
+	defer cancel()
+
+	query := `INSERT INTO movies (title, year, runtime, genres) VALUES ($1, $2, $3, $4)
+	RETURNING id, created_at, version`
+
+	args := []interface{}{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
+	
+	return m.DB.QueryRowContext(ctx, query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
+}
+
+// Get fetching a specific record from the movies table
+func (m MovieModel) Get(id int64) (*Movie, error) {
+
+	if id < 1 {
+		return nil, ErrRecordNotFound
+	}
+
+	query := `SELECT id, created_at, title, year, runtime, genres, version
+	FROM movies WHERE id = $1`
+
+	movie := Movie{}
+
+	err := m.DB.QueryRow(query, id).Scan(&movie.ID, &movie.CreatedAt, &movie.Title, &movie.Year, 
+	&movie.Runtime, pq.Array(&movie.Genres), &movie.Version)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	return &movie, nil
+}
+
+// Update updating a specific record in the movies table
+func (m MovieModel) Update(movie *Movie) (error) {
+	query := `UPDATE movies 
+	SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
+	WHERE id = $5
+	RETURNING version`
+
+	args := []interface{}{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres), movie.ID}
+
+	return m.DB.QueryRow(query, args...).Scan(&movie.Version)
+}
+
+// Delete deleting a specific record from the movies table
+func (m MovieModel) Delete(id int64) error {
+
+	if id < 1 {
+		return ErrRecordNotFound
+	}
+
+	query := `DELETE FROM movies WHERE id = $1`
+	result, err := m.DB.Exec(query, id)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return ErrRecordNotFound
+	}
+	
+	return nil
 }
