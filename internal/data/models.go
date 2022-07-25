7d811bc52dd56a1180bec1ef6be3e5e16ee7b1a3
hafizmfadli <lshafizmf1st@gmail.com>
2022-07-25 14:48:27 +0700

    add method for retrieve details of user associated with activation token

--- internal/data/models.go
+++ internal/data/models.go
@@ -3,6 +3,7 @@ package data
 import (
 	"database/sql"
 	"errors"
+	"time"
 )
 
 var (
@@ -25,6 +26,12 @@ type Models struct {
 		Insert(user *User) error
 		GetByEmail(email string) (*User, error)
 		Update(user *User) error
+		GetForToken(tokenScope, tokenPlaintext string) (*User, error)
+	}
+	Tokens interface {
+		Insert(token *Token) (error)
+		DeleteAllForUser(scope string, userID int64) error
+		New(userID int64, ttl time.Duration, scope string) (*Token, error)
 	}
 }
 
@@ -33,5 +40,6 @@ func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
 		Users: UserModel{DB: db},
+		Tokens: TokenModel{DB: db},
 	}
 }
